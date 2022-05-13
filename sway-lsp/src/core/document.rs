e106ddd29e8bda9c9a280a44f53091fef5b668fc
JoshuaBatty <joshpbatty@gmail.com>
2022-05-13 14:40:44 +1000

    wip

--- sway-lsp/src/core/document.rs
+++ sway-lsp/src/core/document.rs
@@ -84,6 +84,8 @@ impl TextDocument {
         self.clear_tokens();
         self.clear_hash_maps();
 
+        self.parse_typed_tokens_from_text();
+
         match self.parse_tokens_from_text() {
             Ok((tokens, diagnostics)) => {
                 self.store_tokens(tokens);
@@ -107,6 +109,54 @@ impl TextDocument {
 
 // private methods
 impl TextDocument {
+    fn parse_typed_tokens_from_text(&self) {
+        use sway_core::{
+            BuildConfig,
+            CompileResult,
+            TypedParseTree,
+            semantic_analysis::{
+                namespace, Namespace,
+            },
+        };
+
+        let text = Arc::from(self.get_text());
+        let parse_tree = parse(text, None).value.unwrap();
+
+        let program_type = &parse_tree.tree_type;
+        let file_name = std::path::PathBuf::from(self.get_uri());
+        let build_config = BuildConfig::root_from_file_name_and_manifest_path(
+            file_name,
+            Default::default(),
+        );
+        let mut dead_code_graph = Default::default();
+        let mut namespace = Namespace::init_root(namespace::Module::default());
+
+        let CompileResult {
+            value: typed_parse_tree_result,
+            warnings: _new_warnings,
+            errors: _new_errors,
+        } = TypedParseTree::type_check(
+            parse_tree.tree,
+            &mut namespace,
+            program_type,
+            &build_config,
+            &mut dead_code_graph,
+        );
+        eprintln!("typed_parse_tree_result: {:#?}", typed_parse_tree_result);
+
+        if let Some(typed_parse_tree) = typed_parse_tree_result {
+            match typed_parse_tree {
+                TypedParseTree::Script{ main_function, namespace, declarations, all_nodes } => {
+                    //eprintln!("main_function: {:#?}", main_function);
+                    //eprintln!("namespace: {:#?}", namespace);
+                    //eprintln!("declarations: {:#?}", declarations);
+                    eprintln!("all_nodes: {:#?}", all_nodes);
+                }
+                _ => (),
+            }
+        }
+    }
+
     fn parse_tokens_from_text(&self) -> Result<(Vec<Token>, Vec<Diagnostic>), Vec<Diagnostic>> {
         let text = Arc::from(self.get_text());
         let parsed_result = parse(text, None);
